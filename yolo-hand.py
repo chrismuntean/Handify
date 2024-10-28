42968f6bf797fe70e28eb74f1772e6d00bef4dc7
Chris Muntean <chrismunt123@gmail.com>
2024-10-28 03:00:34 -0700

    Started model training

--- yolo-hand.py
+++ yolo-hand.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 # Load the model and run inference on the webcam
-YOLO('best.pt').predict(source=0, show=True)
+YOLO('runs/pose/train6/weights/best.pt').predict(source=0, show=True)
 
 # test commit
