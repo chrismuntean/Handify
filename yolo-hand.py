47c2952451c0ad9f1a58cb9905d4d2426774a715
Chris Muntean <chrismunt123@gmail.com>
2024-11-04 01:33:36 -0700

    Removed yolo stuff

--- yolo-hand.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-# Load the model and run inference on the webcam
-# YOLO('models/best.pt').predict(source=0, show=True)
-YOLO('models/yolo11n-pose-hands.pt').predict(source=0, show=True)
