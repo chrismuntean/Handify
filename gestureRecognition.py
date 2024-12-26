769fb661acf1bb2f59ed1274ff451191f2e9a122
Chris Muntean <chrismunt123@gmail.com>
2024-12-26 00:33:48 -0700

    Removed excess console logging and cleaned up error printing

--- gestureRecognition.py
+++ gestureRecognition.py
@@ -24,7 +24,7 @@ h = 0
 def gen_frames(session_data):
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
-        print("Error: Could not open webcam.")
+        print("[ERROR] Could not open webcam.")
         return
 
     with mp_hands.Hands(
--- static/get-current-song.js
+++ static/get-current-song.js
@@ -9,7 +9,7 @@ function fetchCurrentSong() {
         fetch('/current-song-request')
             .then(response => {
                 if (!response.ok) {
-                    console.error('Error fetching song data:', response.statusText);
+                    console.error('[ERROR] Issue fetching song data:', response.statusText);
                     return;
                 }
                 return response.json();
@@ -30,9 +30,6 @@ function fetchCurrentSong() {
                 }
             })
             .catch(console.error);
-    } else {
-        // Spotify is not ready, skip the fetch
-        console.log('Spotify is not ready for API calls.');
     }
 }
 
